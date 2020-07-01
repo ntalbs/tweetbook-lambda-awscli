3a55dc3844f14fb8f2764c7af9ac56aedd457d81
ntalbs <ntalbsen@gmail.com>
2020-07-01 11:34:37 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,38 @@
 #!/usr/bin/env bash
 
-zip -r function.zip .
+# This will create tweet-book lambda function. The following environment variables should be set before running this script.
+#
+# - TWITTER_CONSUMER_KEY
+# - TWITTER_CONSUMER_SECRET
+# - TWITTER_ACCESS_TOKEN
+# - TWITTER_ACCESS_TOKEN_SECRET
+#
+# The IAM role used here is created manually.
 
-aws lambda update-function-code --function-name tweetBook --zip-file fileb://function.zip
+LAMBDA_FUNCTION_NAME='tweet-book'
+
+function update-lambda {
+    aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://function.zip
+}
+
+function create-lambda {
+    aws lambda create-function \
+        --function-name $LAMBDA_FUNCTION_NAME \
+        --runtime nodejs12.x \
+        --zip-file fileb://function.zip \
+        --handler index.handler \
+        --role arn:aws:iam::884307244203:role/tweet-book-role
+}
+
+zip -r function.zip . -x "*.sh" -x ".git/*" -x "README.md" -x "*.zip"
+
+update-lambda
+
+if [ $? -ne 0 ]; then
+    echo "Create function $LAMBDA_FUNCTION_NAME"
+    create-lambda
+fi
+
+aws lambda update-function-configuration --function-name $LAMBDA_FUNCTION_NAME --environment "Variables={TWITTER_CONSUMER_KEY=$TWITTER_CONSUMER_KEY,TWITTER_CONSUMER_SECRET=$TWITTER_CONSUMER_SECRET,TWITTER_ACCESS_TOKEN=$TWITTER_ACCESS_TOKEN,TWITTER_ACCESS_TOKEN_SECRET=$TWITTER_ACCESS_TOKEN_SECRET}"
 
 rm function.zip
